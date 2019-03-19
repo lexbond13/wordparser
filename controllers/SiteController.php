ff388546df72940037040836ff779d0426aa42c8
Lex Bond <exbond@mail.ru>
2019-03-19 22:23:54 +0700

    some

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -36,6 +36,19 @@ class SiteController extends Controller
         ];
     }
 
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
 
     /**
      * Displays homepage.
